bbb6f8999e0802c591dd2e45f291002ba5006921
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-02 15:17:24 -0700

    put the navbar on the left-hand side instead of the top

--- app/casemanagement.component.ts
+++ app/casemanagement.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'casemanagement',
-    template: '<h1>Case Management Component</h1>'
+    template: '<h1>Case Management</h1>'
 })
 export class CaseManagementComponent {}
