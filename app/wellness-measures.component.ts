4d7944e844a6c0132572325b4dc3b2cb4d184532
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-03 11:48:57 -0700

    add an axis which displays metric names to wellness measures

--- app/wellness-measures.component.ts
+++ app/wellness-measures.component.ts
@@ -17,20 +17,38 @@ export class WellnessMeasuresComponent implements OnInit{
     ]
 
     ngOnInit(): void {
+        console.log(this.data.map(function(d) { return d.metricName }))
         //Draw D3 Graph
             let margin = {
-                left: 20,
+                left: 200,
                 right: 20,
                 bottom: 20,
                 top: 20
             }
             let width = 400,
                 height = 300;
-            let svg = d3.select(".wellness-measures")
+                    console.log(width - margin.left - margin.right)
+
+            let graph = d3.select(".wellness-measures")
                 .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            let xScale = d3.scaleLinear()
+                .domain(0, 100)
+                .range([0, width - margin.left - margin.right]);
+            //categorical scale
+            let catScale = d3.scaleBand()
+                .domain(this.data.map(function(d) { return d.metricName }))
+                .range([0, width - margin.top - margin.bottom]);
+            // Define Axes
+            let yAxis = d3.axisLeft()
+                .scale(catScale)
             
+            graph.append("g")
+                .attr("class", "axis")
+                .call(yAxis);
             // Add scales for the axes
             
             // 
