04f409e7817596542a9271235a5552a0ca6da36c
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-07 12:06:14 -0800

    fix a bug in the typing dependency

--- app/recommendation-table.component.ts
+++ app/recommendation-table.component.ts
@@ -2,8 +2,105 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'recommendation-table',
-    template: `recommendation table`
+    template: `
+        <table>
+            <tr>
+                <th>Exclude</th>
+                <th>Recommended Order</th>
+                <th>Order By</th>
+                <th>Problem</th>
+                <th>Medicare CQM</th>
+                <th>Clinical Guideline Publisher</th>
+            </tr>
+            <tr *ngFor="let datum of data">
+                <td></td>
+                <td>{{datum.recOrder}}</td>
+                <td>{{datum.orderBy}}</td>
+                <td><a href={{datum.problemUrl}}>{{datum.problem}}</a></td>
+                <td>{{datum.cqmName}}</td>
+                <td>{{datum.guidelinePublisher}}</td>
+            </tr>
+        </table>
+    `
 })
 export class RecommendationTableComponent {
-
+    data = [
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+        {
+            recOrder: "Hemoblobin A1C",
+            orderBy: Date.parse("June 15, 2016"),
+            problem: "Diabetes mellitus II",
+            //We will talk later about how to implement
+            //the dynamic routing in the problemUrl
+            cqmName: "CMS122",
+            cqmUrl: "",
+            guidelinePublisher: "CMS",
+            guidelinePublisherUrl: ""
+        },
+    ]
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="modules/d3-axis/index.d.ts" />
 /// <reference path="modules/d3/index.d.ts" />
