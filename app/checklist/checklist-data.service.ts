6d6daf51d808eb2c6e038924980fb4ce34cd5587
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-08 10:49:04 -0800

    rename the recommendation table service the checklist data service

--- app/checklist/checklist-data.service.ts
+++ app/checklist/checklist-data.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { RECOMMENDATIONS } from './mock-recommendations';
 
 @Injectable()
-export class RecommendationTableService {
+export class ChecklistDataService {
     getRecommendations(): Promise<any[]> {
         return Promise.resolve(RECOMMENDATIONS);
     }
