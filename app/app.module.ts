d7ccdd4aab289aca634a5055ba8134917e793cc6
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-08 10:14:01 -0800

    move all checklist-related concerns into a checklist folder

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
-import { ChecklistComponent } from './checklist.component';
+import { ChecklistComponent } from './checklist/checklist.component';
 import { CaseManagementComponent } from './casemanagement.component';
 import { HomeComponent } from './home.component';
 import { RXDataComponent } from './rx-data.component';
 import { ValuePropComponent } from './value-prop.component';
-import { PatientSummaryComponent } from './patient-summary.component';
-import { WellnessMeasuresComponent } from './wellness-measures.component';
-import { RecommendationTableComponent } from './recommendation-table.component';
+import { PatientSummaryComponent } from './checklist/patient-summary.component';
+import { WellnessMeasuresComponent } from './checklist/wellness-measures.component';
+import { RecommendationTableComponent } from './checklist/recommendation-table.component';
 
 @NgModule({
   imports:      [ 
