38962957fbcd73afaaddf4fc7926c69a4131f9e3
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-09 10:00:10 -0800

    refactor the data out of the patient-sum component  into a mock file served with a service

---
+++ app/checklist/mock-wellness-measures.ts
@@ -0,0 +1,9 @@
+export const WELLNESSMEASURES = [
+        { metricName: "Quality Payment Program", metricScore: 70 },
+        { metricName: "Preventive Medicine", metricScore: 60 },
+        { metricName: "Type II diabetes mellitus", metricScore: 50 },
+        { metricName: "Chronic Kidney Disease", metricScore: 40 },
+        { metricName: "Congestive Heart Failure", metricScore: 30 },
+        { metricName: "Coronary Artery Disease", metricScore: 20 },
+        { metricName: "Asthma", metricScore: 10 }
+    ]
