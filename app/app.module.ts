daa52b4318885cf230450c94ca25763474b1cf8e
Bhawani Parajuli <parajulibhawani@gmail.com>
2016-11-11 09:40:05 -0800

    merged casemanagement branch.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { FormsModule }   from '@angular/forms';
-import { RxDataService } from './rx-data.service';
-import { HttpModule }    from '@angular/http';
 
-=======
+import { RxDataService } from './rx-data.service';
+
 import {FormsModule} from "@angular/forms";
 import { HttpModule, JsonpModule  }    from '@angular/http';
->>>>>>> casemanagement
+
 import { AppRoutingModule } from './app-routing.module';
 
 // Imports for loading/configuring in-memory-web-api
@@ -16,17 +13,15 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryRxDataService }  from './in-memory-rxdata.service';
 
 import { AppComponent }  from './app.component';
-<<<<<<< HEAD
-import { CaseManagementComponent } from './casemanagement.component';
-=======
-import { ChecklistComponent } from './checklist.component';
+
+
 import { CaseManagementComponent } from './CaseManagement/casemanagement.component';
->>>>>>> casemanagement
+import {CaseManagementService} from './CaseManagement/casemanagement.service';
 import { HomeComponent } from './home.component';
 import { RXDataComponent } from './rx-data.component';
 import { ValuePropComponent } from './value-prop.component';
 
-<<<<<<< HEAD
+
 import { ChecklistComponent } from './checklist/checklist.component';
 import { PatientSummaryComponent } from './checklist/patient-summary.component';
 import { WellnessMeasuresComponent } from './checklist/wellness-measures.component';
@@ -40,17 +35,8 @@ import { SelfAssessmentComponent } from './self-assessment.component';
     AppRoutingModule,
     FormsModule,
     HttpModule,
+    JsonpModule ,
     InMemoryWebApiModule.forRoot(InMemoryRxDataService)
-=======
-import {CaseManagementService} from './CaseManagement/casemanagement.service';
-@NgModule({
-  imports:      [ 
-    BrowserModule, 
-    AppRoutingModule,
-    FormsModule,
-    HttpModule,
-    JsonpModule 
->>>>>>> casemanagement
      ],
   declarations: [
     AppComponent,
@@ -58,21 +44,14 @@ import {CaseManagementService} from './CaseManagement/casemanagement.service';
     CaseManagementComponent,
     HomeComponent,
     RXDataComponent,
-<<<<<<< HEAD
+
     ValuePropComponent,
     PatientSummaryComponent,
     WellnessMeasuresComponent,
     RecommendationTableComponent,
     SelfAssessmentComponent
    ],
-   providers: [ RxDataService ],
+   providers: [ RxDataService, CaseManagementService ],
    bootstrap:    [ AppComponent ]
-=======
-    ValuePropComponent
-    
-   ],
-   providers: [CaseManagementService],
-  bootstrap:    [ AppComponent ]
->>>>>>> casemanagement
 })
 export class AppModule { }
