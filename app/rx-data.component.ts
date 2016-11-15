fa4cf72acc7900e63e148f003c5b9e5387a88ca0
Bo Zhao <zhaobo419@gmail.com>
2016-11-15 15:27:39 -0800

    add bar chart on rx data

--- app/rx-data.component.ts
+++ app/rx-data.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { RxRecord }          from './rx-record.model';
 import { RxDataService }     from './rx-data.service';
 
+import * as d3 from 'd3';
+import * as d3Scale from 'd3-scale';
+import * as d3Axis from 'd3-axis';
+
 @Component({
     moduleId: module.id,
     selector: 'rx-data',
@@ -15,6 +19,8 @@ import { RxDataService }     from './rx-data.service';
           {{record.rxName}}, {{record.ptId}}, {{record.drId}}, {{record.state}}, {{record.city}}, {{record.zip}}
         </li>
       </ul>
+
+      <div class="rx-data"></div>
     `,
     providers: [RxDataService]
 })
@@ -24,18 +30,74 @@ export class RXDataComponent implements OnInit {
 
   constructor(private rxDataService: RxDataService) {}
 
+
+
+
+  static drawGraph(data: any[]): void{
+      let margin = {
+              left: 200,
+              right: 20,
+              bottom: 20,
+              top: 20
+          }
+          let width = 400,
+              height = 300;
+
+          let graph = d3.select(".rx-data")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          // Add scales for the axes (ignore intellisense, this works fine)
+          let yScale = d3Scale.scaleLinear()
+              .domain([0, d3.max(data.map(function(d) { return d.drId } ))])
+              .range([height, 0]);
+          //categorical scale
+          let categories = data.map(function(d) { return d.ptId });
+          let catScale = d3Scale.scaleBand()
+              .domain(categories)
+              .range([0, height])
+              .paddingInner(0.4)
+              .paddingOuter(0.2);
+
+          // Define Axes
+          let yAxis = d3Axis.axisLeft(yScale)
+              .tickSize(0);
+
+          let xAxis = d3Axis.axisBottom(catScale);
+
+          let vertGuide = graph.append("g")
+              .attr("class", "axis")
+          yAxis(vertGuide);
+
+          let horiGuide = graph.append('g').attr('class',"axis")
+            .attr("transform", "translate(" + 0 + "," + height + ")");
+          xAxis(horiGuide);
+          // Draw rectangles
+          graph.selectAll("rect")
+              .data(data)
+              .enter().append("rect")
+              .attr("width", catScale.bandwidth())
+              .attr("fill", "blue")
+              .attr("x", function(d) {
+                  return catScale(d.ptId);
+              })
+              .attr("y", function(d) { return height - yScale(d.drId) })
+              .attr("height", function(d) { return yScale(d.drId) });
+  }
   ngOnInit(): void {
-    this.getRxData();
+      this.rxDataService.getRxData()
+          .then(function(d) {
+              RXDataComponent.drawGraph(d);
+          })
   }
 
-  /* service - pre-promise -- working */
-  // getRxData(): void {
-    // this.rxdata = this.rxDataService.getRxData();
-    // }
 
-  /* service w/promise */
-  getRxData(): void {
-    this.rxDataService.getRxData().then(rxdata => this.rxdata = rxdata);
-  }
+
+
+
+
+
 
 }
