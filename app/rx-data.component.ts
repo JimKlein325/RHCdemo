05c503ce818566269385723f14873c91388a4bb2
James Turello <jturello@yahoo.com>
2016-11-14 20:21:27 -0800

    clean rx-data.component code

--- app/rx-data.component.ts
+++ app/rx-data.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RxRecord }          from './rx-record.model';
 import { RxDataService }     from './rx-data.service';
-// import { RxDataFilter }   from './rx-data-filter.component';
 import { RxDataFilterPipe }  from './rx-data-filter.pipe';
 
 @Component({
@@ -13,30 +12,26 @@ import { RxDataFilterPipe }  from './rx-data-filter.pipe';
 
       <h3>Drug Class: <span class='drugClass'>Oncologics</span></h3>
 
-    <!--  <rx-data-filter>filter...</rx-data-filter>  -->
+      <h4>Filter Data By...</h4>
+      <form  #filterOptionsForm="ngForm">
+        <div class='row'>
+          <div class='col-sm-2'>
+            <select #modality style="width: 100%">
+              <option value="" selected="selected"></option>
+              <option value="state">State</option>
+              <option value="city">City</option>
+              <option value="zip">Zip Code</option>
+            </select>
+          </div>
+          <div class='col-sm-4'>
+            <input type="text" style="width: 100%" placeholder="Enter text..." #option>
+          </div>
+          <div class='col-sm-2'>
+            <button type='submit'>Submit</button>
+          </div>
+        </div>
+      </form>
 
-<!-- ********************************************************* -->
-<h4>Filter Data By...</h4>
-<form (ngSubmit)="onSubmit()" #filterOptionsForm="ngForm">
-  <div class='row'>
-    <div class='col-sm-2'>
-      <select #modality >
-        <option value="" selected="selected"></option>
-        <option value="state">State</option>
-        <option value="city">City</option>
-        <option value="zip">Zip Code</option>
-      </select>
-    </div>
-    <div class='col-sm-4'>
-      <input type="text" style="width: 100%" placeholder="Enter text..." #option>
-    </div>
-    <div class='col-sm-2'>
-      <button type='submit'>Submit</button>
-    </div>
-  </div>
-</form>
-
-<!-- ********************************************************* -->
 
       <br>
 
@@ -63,22 +58,11 @@ export class RXDataComponent implements OnInit {
   }
 
   /* service - pre-promise -- working */
-  // getRxData(): void {
-    // this.rxdata = this.rxDataService.getRxData();
-    // }
+  // getRxData(): void { this.rxdata = this.rxDataService.getRxData(); }
 
   /* service w/promise */
   getRxData(): void {
     this.rxDataService.getRxData().then(rxdata => this.rxdata = rxdata);
-  }
-
-  onSubmit(modality: string, option: string): void {
-    console.log("onSub: m/o: " + modality + ", " + option);
-    // console.log("onSub: m.val/o.val: " + modality.value + ", " + option.value);
-
-    this.filterModality = modality;
-    this.filterOption = option;
-
   }
 
 }
