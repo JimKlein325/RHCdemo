46c33afec4784a8c52f51e252961ad2bf94aa7e6
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-17 09:48:44 -0800

    remove itunes search

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,7 +26,6 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
       'd3': 'npm:d3/build/d3.min.js',
       'd3-axis': "npm:d3-axis/build/d3-axis.js",
       'd3-scale': 'npm:d3-scale/build/d3-scale.min.js',
