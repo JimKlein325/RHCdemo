6c1a3525e14a9251407f1ec7ee4aac93fad531ff
Ryan Streur <ryan.j.streur@gmail.com>
2016-11-17 12:03:07 -0800

    connect the rx-data service to the path for the .net core API

--- app/DrugCompare/rx-data.service.ts
+++ app/DrugCompare/rx-data.service.ts
@@ -10,22 +10,22 @@ import { RxRecord } from './rx-record.model';
 export class RxDataService {
 
   private headers = new Headers({'Content-Type': 'application/json'});
-  private rxdataUrl = 'app/rxdata';
+  private rxdataUrl = 'http://localhost:62122/api/mockdata/rxdata';
 
   constructor(private http: Http) { }
 
+  private handleError(error: any): Promise<any> {
+    console.error('Error: ' + error.status + " : " + error.statusText , error);
+    return Promise.reject(error.message || error);
+  }
   getRxData(): Promise<RxRecord[]> {
     /* promise / pre-http service -- working  */
     // return Promise.resolve(RXDATA);
 
     return this.http.get(this.rxdataUrl)
                .toPromise()
-               .then(response => response.json().data as RxRecord[])
+               .then(response => response.json() as RxRecord[])
                .catch(this.handleError);
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('Error: ' + error.status + " : " + error.statusText , error);
-    return Promise.reject(error.message || error);
-  }
 }
