0c8a90223b057886bc313fd177c66746d8014d58
Callan McNulty <jabberwocky222@gmail.com>
2016-11-18 16:31:01 -0500

    Add method for formatting raw dose response data

--- app/dose-response-data.service.ts
+++ app/dose-response-data.service.ts
@@ -15,4 +15,25 @@ export class DoseResponseDataService {
   getData() {
     return this.http.get(this.request);
   }
+
+  transformData(rawData: any) {
+    let data: any[];
+    data = [];
+
+    for(let i=0; i<rawData.length; i++) {
+      let name = rawData[i].ClinicalEvent;
+      let dataset = data.find(function(event: any){ return event.name === name; });
+      if(dataset === undefined) {
+        dataset = {name: name, values: []};
+        data.push(dataset);
+      }
+      let dataPoint = [rawData[i].EventDate, rawData[i].ClinicalValue];
+      dataset.values.push(dataPoint);
+      dataset.unit = rawData[i].Units;
+      dataset.refMin = rawData[i].ReferenceRangeMin;
+      dataset.refMax = rawData[i].ReferenceRangeMax;
+    }
+
+    return {title: "Lab Values for Patient " + rawData[0].PatientID, data: data};
+  }
 }
